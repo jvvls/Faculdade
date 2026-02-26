089a5a9e9abbe5b92d0cb24bcf1715fc5b757f7d
João Victor Vial <joaovictorvial@gmail.com>
2026-02-26 19:57:51 -0300

    ex3

---
+++ PM/lista00/src/lista00/ex3.java
@@ -0,0 +1,24 @@
+package lista00;
+import java.util.HashSet;
+import java.util.Set;
+
+
+public class ex3 {
+    public static void main(String[] args) {
+    	
+    	int[] matriculaProgramacao = {212121, 3131231, 12312312, 12312313};
+    	int[] matriculaModelagem = {212121, 32323, 76676, 76767};
+    	
+        Set<Integer> set = new HashSet<>();
+        
+        for (int matricula : matriculaProgramacao) {
+        	set.add(matricula);
+        }
+        
+        for (int matricula : matriculaModelagem){
+        	if(set.contains(matricula)){
+        	System.out.print(matricula);
+        	}
+        }
+    }
+}
